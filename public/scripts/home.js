5686d21cb11dee30a53b4052abbfbf30b0fd622a
oJPBarbosa <contato.ferb@gmail.com>
2021-11-11 14:41:32 -0300

    add home.js

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,16 +1,16 @@
-const homeSignupEmail = document.querySelector('#signup-email');
-const homeSignupAnchor = document.querySelector('#continue-to-signup-anchor');
+const email = document.querySelector('#signup-email');
+const anchor = document.querySelector('#continue-to-signup-anchor');
 
-homeSignupEmail.addEventListener('focus', () => {
-  homeSignupEmail.parentElement.style.border = '2px solid var(--accent-color)';
+email.addEventListener('focus', () => {
+  email.parentElement.style.border = '2px solid var(--accent-color)';
 });
 
-homeSignupEmail.addEventListener('focusout', () => {
+email.addEventListener('focusout', () => {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
  
-  if (re.test(String(homeSignupEmail.value).toLowerCase()))
-    homeSignupAnchor.setAttribute('href', `/signup?email=${homeSignupEmail.value}`);
+  if (re.test(String(email.value).toLowerCase()))
+    homeSignupAnchor.setAttribute('href', `/signup?email=${email.value}`);
 
   else
-    homeSignupEmail.parentElement.style.border = '2px solid #ee5555';
+    anchor.parentElement.style.border = '2px solid #ee5555';
 });
