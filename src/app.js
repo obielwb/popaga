9861bf3345e1b0fe37b69e1889918d190a30b48a
oJPBarbosa <contato.ferb@gmail.com>
2021-11-11 10:37:09 -0300

    add initial stylization

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const routes = require('./routes');
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
+app.use(express.static('public'));
+app.use('/images', express.static('./public/images'));
+app.use('/css', express.static('./public/styles/css'));
+
 app.use(routes);
 
 module.exports = app;
