c927a4be68b163d3099a11261bda0be66c253187
oJPBarbosa <contato.ferb@gmail.com>
2021-11-12 10:38:07 -0300

    improve theme

---
+++ public/scripts/theme.js
@@ -0,0 +1,38 @@
+const root = document.querySelector(':root');
+
+const light = document.querySelector('.fa-sun');
+const dark = document.querySelector('.fa-moon');
+
+const themes = [light, dark];
+
+const reference = {
+  'light': light,
+  'dark': dark,
+};
+
+const inverse = {
+  'light': dark,
+  'dark': light,
+};
+
+themes.forEach(theme => {
+  theme.addEventListener('click', (event) => {
+    const theme = event.target.id;
+    root.classList.remove(localStorage.getItem('theme'));
+    root.classList.add(theme);
+
+    reference[theme].classList.add('hide');
+    inverse[theme].classList.remove('hide');
+
+    localStorage.setItem('theme', theme);
+  });
+});
+
+window.addEventListener('load', () => {
+  if (localStorage.getItem('theme') == null)
+    localStorage.setItem('theme', 'light');
+
+  const theme = localStorage.getItem('theme');
+  reference[theme].classList.add('hide');
+  root.classList.toggle(theme);
+});
