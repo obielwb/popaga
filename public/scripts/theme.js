e392379de82c62d0bd519679e21c50d1bf85a88d
oJPBarbosa <contato.ferb@gmail.com>
2021-11-15 12:12:39 -0300

    improve ui/ux

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,29 +1,29 @@
 const root = document.querySelector(':root');
 
-const light = document.querySelector('.fa-sun');
-const dark = document.querySelector('.fa-moon');
+const light = document.querySelector('#light');
+const dark = document.querySelector('#dark');
 
 const favicon = document.querySelector('link[rel=icon]');
 
 const themes = [light, dark];
 
 const reference = {
-  'light': light,
-  'dark': dark,
+  light: light,
+  dark: dark,
 };
 
 const inverse = {
-  'light': dark,
-  'dark': light,
+  light: dark,
+  dark: light,
 };
 
-themes.forEach(theme => {
+themes.forEach((theme) => {
   theme.addEventListener('click', (event) => {
-    const theme = event.target.id;
+    const theme = event.target.dataset.theme;
 
     root.classList.remove(localStorage.getItem('theme'));
     root.classList.add(theme);
-    favicon.href = `/images/favicons/${theme}.svg`;
+    favicon.href = `/favicon/${theme}.svg`;
 
     reference[theme].classList.add('hide');
     inverse[theme].classList.remove('hide');
@@ -33,15 +33,17 @@ themes.forEach(theme => {
 });
 
 window.addEventListener('load', () => {
-  if (localStorage.getItem('theme') == null || 
-      localStorage.getItem('theme') != 'light' && 
+  if (
+    localStorage.getItem('theme') == null ||
+    (localStorage.getItem('theme') != 'light' &&
       localStorage.getItem('theme') != 'dark')
-        localStorage.setItem('theme', 'light');
+  )
+    localStorage.setItem('theme', 'light');
 
   const theme = localStorage.getItem('theme');
 
   reference[theme].classList.add('hide');
 
   root.classList.toggle(theme);
-  favicon.href = `/images/favicons/${theme}.svg`;
+  favicon.href = `/favicon/${theme}.svg`;
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 // TODOS:
 //  - fix sign up issue
-//  - make /login and /signup impossible to access when logged
 //  - add error handling to api responses
 const express = require('express');
 const app = express();
@@ -17,7 +16,7 @@ app.set('views', './src/views');
 
 app.use(express.static('public'));
 app.use('/scripts', express.static('./public/scripts'));
-app.use('/images', express.static('./public/images'));
+app.use('/favicon', express.static('./public/favicon'));
 app.use('/css', express.static('./public/styles/css'));
 
 app.use(routes);
