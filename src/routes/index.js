ff55dcf2d9e3034db627a4786549e492fa7ea575
Gabriel Willian <obielwb@gmail.com>
2021-11-24 16:45:48 -0300

    add payments route to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const routes = express.Router();
 
 const home = require('./home');
 const app = require('./app');
+const payments = require('./payments');
 const signup = require('./signup');
 const login = require('./login');
 
 routes.use('/', home);
 routes.use('/app', app);
+routes.use('/payments', payments)
 routes.use('/signup', signup);
 routes.use('/login', login);
 
